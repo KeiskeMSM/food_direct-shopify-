1ddb259e544c826da2cc1e11eba237660bc22a27
八幡塩 <mishima@yawatashionoMacBook-Pro.local>
2022-04-30 10:40:26 +0900

    4th commit

---
+++ assets/my-script.js
@@ -0,0 +1,13 @@
+// 年齢確認チェックボックス
+$(function () {
+	if ($("div").hasClass("cart__check")) {
+		$(".cart__checkout-button").attr("disabled", "disabled");
+		$("#cart_check").click(function () {
+			if ($(this).prop("checked") == false) {
+				$(".cart__checkout-button").attr("disabled", "disabled");
+			} else {
+				$(".cart__checkout-button").removeAttr("disabled");
+			}
+		});
+	}
+});
